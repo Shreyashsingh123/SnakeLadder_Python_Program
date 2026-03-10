302503cb1e1814d19a80aaf47d2bcd0d60b72d97
Shreyashsingh123 <singhshreyash631@gmail.com>
2026-03-10 15:40:31 +0530

    Added diceroll function using random method

--- SnakeLadder.py
+++ SnakeLadder.py
@@ -1,3 +1,8 @@
+import random
 print("Welcome to Snakes ladder problem")
 position=0
 print("Single player start at position:",position)
+
+def diceroll():
+    dice=random.randint(1,6)
+    return dice
