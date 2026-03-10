f1b25c04259f2502011736fe27ae6665c7a55c4c
Shreyashsingh123 <singhshreyash631@gmail.com>
2026-03-10 15:55:35 +0530

    Added: checks for option either Ladder,snake or no move

--- SnakeLadder.py
+++ SnakeLadder.py
@@ -6,3 +6,15 @@ print("Single player start at position:",position)
 def diceroll():
     dice=random.randint(1,6)
     return dice
+def randomoption(dice):
+    global position
+    opt=random.randint(1,3)#option
+    if (opt==2):
+        print("Ladder")
+        position+=dice
+    elif (opt==1):
+        print("No move")
+    else:
+        print("Snake")
+        position-=dice
+    return position
