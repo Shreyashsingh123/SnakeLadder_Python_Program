ee25b36908c842258c4ba8e8f16a6328e6d17049
Shreyashsingh123 <singhshreyash631@gmail.com>
2026-03-10 16:18:10 +0530

    Added: Until player reaches winning position 100 and if position is negative start from 0

--- SnakeLadder.py
+++ SnakeLadder.py
@@ -10,11 +10,19 @@ def randomoption(dice):
     global position
     opt=random.randint(1,3)#option
     if (opt==2):
-        print("Ladder")
         position+=dice
     elif (opt==1):
-        print("No move")
+        # print("No move")
+        pass
     else:
-        print("Snake")
         position-=dice
+        if(position<0):#if position is negative then start from 0
+            position=0
     return position
+
+while(position<100):# until player1 reaches 100(win) it will execute
+    dice=diceroll()
+    position=randomoption(dice)
+
+if(position>=100):
+    print("Player1 win")
